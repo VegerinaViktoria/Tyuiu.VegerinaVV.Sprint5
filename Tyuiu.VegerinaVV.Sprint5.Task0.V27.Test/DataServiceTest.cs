73da7f1a7f1cc62bef13c04aca4004b09f411bc2
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-21 21:49:42 +0500

    Add project files.

---
+++ Tyuiu.VegerinaVV.Sprint5.Task0.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.VegerinaVV.Sprint5.Task0.V27.Lib;
+namespace Tyuiu.VegerinaVV.Sprint5.Task0.V27.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\Users\vvvik\AppData\Local\Temp\OutputFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint5.Task0.V27/Program.cs
@@ -0,0 +1,42 @@
+﻿using Tyuiu.VegerinaVV.Sprint5.Task0.V27.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл.                       *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #27                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+        Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль. Округлить до   *");
+        Console.WriteLine("* трёх знаков после запятой.                                              *");
+        Console.WriteLine("*                                                                         *");
+
+        int x = 3;
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("x = " + x);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        string res = ds.SaveToFileTextData(x);
+
+        Console.WriteLine("Файл: " + res);
+        Console.WriteLine("Создан!");
+        Console.ReadKey();
+
+    }
+}
