d025342f65b16e1bcb89fd3c220fc41015bb5b3c
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-22 23:25:03 +0500

    Task4

---
+++ Tyuiu.VegerinaVV.Sprint5.Task4.V16.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Tyuiu.VegerinaVV.Sprint5.Task4.V16.Lib;
+namespace Tyuiu.VegerinaVV.Sprint5.Task4.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedExistsFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V16.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint5.Task4.V16/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.VegerinaVV.Sprint5.Task4.V16.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение данных из текстового файла.                                *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #16                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Прочитать значение из файла и подставить вместо Х в формуле             *");
+        Console.WriteLine("* y = cos(x) + x ^ 2 - (2*x)/1.2 Вычислить значение по формуле            *");
+        Console.WriteLine("* (Полученное значение округлить до трёх знаков после запятой) и вернуть  *");
+        Console.WriteLine("* полученный результат на консоль.                                        *");
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask4V16.txt";
+
+        Console.WriteLine("Данные находятся в файле " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
