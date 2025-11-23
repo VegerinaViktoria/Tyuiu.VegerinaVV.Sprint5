0e83e7d8f56add8970d69869395c23190876f8b9
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-23 21:44:49 +0500

    Task7

---
+++ Tyuiu.VegerinaVV.Sprint5.Task7.V13.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.VegerinaVV.Sprint5.Task7.V13.Lib
+{
+    public class DataService : ISprint5Task7V13
+    {
+        public string LoadDataAndSave(string path)
+        {
+            string pathSaveFile = Path.Combine(new string[] { Path.GetTempPath(), "OutPutDataFileTask7V13.txt" });
+
+            FileInfo fileInfo = new FileInfo(pathSaveFile);
+            bool fileExists = fileInfo.Exists;
+
+            if (fileExists)
+            {
+                File.Delete(pathSaveFile);
+            }
+
+            string strLine = "";
+            using (StreamReader reader= new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        if (!(line[i] >= 'a' && line[i] <= 'z'))
+                        {
+                            strLine = strLine + line[i];
+                        }
+                    }
+
+                    File.AppendAllText(pathSaveFile, strLine + Environment.NewLine);
+                    strLine = "";
+                }
+            }
+            return pathSaveFile;
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint5.Task7.V13/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.VegerinaVV.Sprint5.Task7.V13.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту.                *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #13                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Удалить все строчные латинские буквы из файла.Полученный результат      *");
+        Console.WriteLine("* сохранить в файл OutPutDataFileTask7V13.txt.                            *");
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask7V13.txt";
+        string pathSaveFile = @"C:\Users\vvvik\AppData\Local\Temp\OutPutDataFileTask7V13.txt";
+        Console.WriteLine("Данные находятся в файле: " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Находится в файле: ");
+        pathSaveFile = ds.LoadDataAndSave(path);
+        Console.WriteLine(pathSaveFile);
+        Console.ReadKey();
+    }
+}
