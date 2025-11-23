b562d5c5b407dab53400441cc60b08289e530c18
Tyuiu.VegerinaVV@outlook.com <vvviktoriavvvegerina@gmail.com>
2025-11-23 19:02:39 +0500

    Task5

---
+++ Tyuiu.VegerinaVV.Sprint5.Task5.V14.Lib/DataService.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.VegerinaVV.Sprint5.Task5.V14.Lib
+{
+    public class DataService : ISprint5Task5V14
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double max3 = 0;
+
+            string list = File.ReadAllText(path);
+            string[] numbers = list.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string str in numbers)
+            {
+                double res = Math.Round(Convert.ToDouble(str.Replace('.', ',')), 3);
+                if (res % 3 == 0 && res > max3)
+                {
+                    max3 = res;
+                }
+            }
+            return GetFactorial(Convert.ToInt32(max3));
+
+        }
+        private double GetFactorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Факториал не определен для отрицательных чисел.", nameof(n));
+            }
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            double result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.VegerinaVV.Sprint5.Task5.V14/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.VegerinaVV.Sprint5.Task5.V14.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #5 | Выполнила: Вегерина В.В. | ИИПб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #5                                                               *");
+        Console.WriteLine("* Тема: Чтение набора данных из текстового файла.                         *");
+        Console.WriteLine("* Задание #5                                                              *");
+        Console.WriteLine("* Вариант #14                                                             *");
+        Console.WriteLine("* Выполнила: Вегерина Виктория Вячеславовна | ИИПб-25-1                   *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Найти факториал наибольшего целого числа, которое делится на 3, в       *");
+        Console.WriteLine("* файле. Полученный результат вывести на консоль. У вещественных значений *");
+        Console.WriteLine("* округлить до трёх знаков после запятой.                                 *");
+        Console.WriteLine("*                                                                         *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        string path = @"C:\DataSprint5\InPutDataFileTask5V14.txt";
+
+        Console.WriteLine("Данные находятся в файле " + path);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        double res = ds.LoadFromDataFile(path);
+        Console.WriteLine("Факториал наибольшего целого числа, которое делится на 3 = " + res);
+        Console.ReadKey();
+    }
+}
